48c1b24b687db69377be47b7e2f05adcd7c3f506
eliteSCHW31N <business@eliteschw31n.de>
2021-08-14 19:32:13 +0200

    bugfix: parse correct value

--- src/components/NotificationHandler.vue
+++ src/components/NotificationHandler.vue
@@ -77,7 +77,7 @@ export default {
                     userstate['display-name']
                 )
                 .replace(
-                    /(\${months})/g,
+                    /(\${month})/g,
                     cumulativeMonths
                 )
 
