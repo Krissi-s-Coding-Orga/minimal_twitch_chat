32af79f9fc8f790d0c358c9291af1ce55a61a8ae
eliteSCHW31N <admin@eliteschw31n.de>
2021-08-23 17:55:55 +0200

    convert string to Number

--- src/components/NotificationHandler.vue
+++ src/components/NotificationHandler.vue
@@ -29,7 +29,7 @@ export default {
     },
     methods: {
         getTimeout: () => {
-            return localStorage.notificationTimeout
+            return Number(localStorage.notificationTimeout)
         },
         removeNotification: (id) => {
             setTimeout(() => {
