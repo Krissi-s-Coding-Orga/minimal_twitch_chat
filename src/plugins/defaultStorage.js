809248dd5b34494fdc08de7474cc1fda5cbbd4be
eliteSCHW31N <admin@eliteschw31n.de>
2021-09-09 17:59:22 +0200

    bugfix: add channel, fix localstorage check

--- src/plugins/defaultStorage.js
+++ src/plugins/defaultStorage.js
@@ -5,13 +5,17 @@ const map = require('./storageMap.json')
 export default { loadDefaults }
 
 function loadDefaults() {
+    console.log('load defaults')
     for(const key in map) {
-        if(typeof (localStorage.getItem(key)) !== 'undefined') {
+        if(localStorage.getItem(key) !== null) {
             return
         }
         const path = map[key]
         const splitPath = path.split('.')
         const section = config[splitPath[0]][splitPath[1]]
+
+        console.log(path)
+        console.log(section)
 
         localStorage.setItem(key, section)
     }
